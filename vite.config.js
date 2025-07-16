244f1c3c475e5eb173abb78803908aa0abca5d0e
Saidu45 <saiduth200@gmail.com>
2025-07-16 11:27:22 +0000

    UPDATED FILES

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://saidu45.github.io/vite-repo/', // Set this to your GitHub Pages repo name
+  base: '/vite-repo/'
 })
